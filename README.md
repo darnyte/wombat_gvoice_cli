11bf7755e00a386242a6db111d1b5946d31c76ac
uriel1998 <steven.saus@gmail.com>
2013-10-01 19:23:55 -0400

    Initial commit of files

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ wombat_gvoice_cli
 =================
 
 Building on node.js voice.js framework for the pure purpose of cli-based sms messaging, nothing more.
+
+Requires:
+
+colors.js - https://github.com/marak/colors.js
+voice.js - https://github.com/amper5and/voice.js
+
+Much is derived from voice.js examples and from gv.app
+gv.app - https://github.com/matthewp/gv-app
+
+
---
+++ get-sms.js
@@ -0,0 +1,44 @@
+//heavily edited from the voice.js example, requiring more dependencies and the like.
+
+var voicejs = require('./voice.js');
+var colors = require('./colors');
+
+
+var client = new voicejs.Client({
+	email: process.argv[2] || 'email@gmail.com',
+	password: process.argv[3] || 'password',
+	tokens: require('./tokens.json')
+});
+
+// Get the 5 latest sms conversations and display their threads, from first text to last
+client.get('sms', {limit:5}, function(error, response, data){
+	if(error){
+		return console.log(error);
+	}
+	
+	if(!data || !data.conversations_response || !data.conversations_response.conversationgroup){ return console.log('No conversations.')}
+	
+	console.log('SMS: Latest conversations.');
+	data.conversations_response.conversationgroup.forEach(function(convo){
+		//kludge to limit number of messages presented in each convo
+		var i = 0;
+		console.log('\n', convo.conversation.status == 1 ? ' ' : '+', new Date(convo.conversation.conversation_time).toDateString(), convo.call[0].phone_number);
+		convo.call.reverse().forEach(function(msg){
+			if (i < 3){
+				// message type 11 - from me
+				// message type 10 - from them
+				if (msg.type == 11){
+					console.log(new Date(msg.start_time).toLocaleTimeString().replace(/[ZT]/g,' ').substr(0,16),' ',msg.message_text.grey);
+					} else {
+					console.log(new Date(msg.start_time).toLocaleTimeString().replace(/[ZT]/g,' ').substr(0,16),' ',msg.message_text.cyan);
+					}
+
+//				console.log(new Date(msg.start_time).toISOString().replace(/[ZT]/g,' ').substr(0,16), msg.message_text, msg.type);
+				}
+			i++;
+
+
+		});
+		
+	});
+});
