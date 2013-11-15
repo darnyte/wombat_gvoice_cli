dcfa141d5639198fb971b3a5d398d33981a8ec97
uriel1998 <steven.saus@gmail.com>
2013-11-14 22:53:25 -0500

    completed commandline switches and reply with --interactive switch.

--- get-sms.js
+++ get-sms.js
@@ -17,37 +17,44 @@ var client = new voicejs.Client({
 //Array for replying, marking read, etc.
 var ids = [];
 
-if (process.argv.indexOf('--markread') > -1) {
-	//this means that we will automatically mark things as read
-	console.log('Marking Read');
-}
-
 if (process.argv.indexOf('--getread') > -1) {
 	var which = 'sms';
 	} else {
 	var which = 'unread';
 	}
 
+var markread = 0;
+if (process.argv.indexOf('--markread') > -1) {
+	markread = process.argv.indexOf('--markread');
+	console.log('Auto-Marking Read'.bold.red);
+}
+
 //Max number of threads to retrieve
 if (process.argv.indexOf('--maxthreads') > -1) {
 	var placeholder = process.argv.indexOf('--maxthreads');
 	placeholder++;
-	var maxthreads = parseFloat(process.argv[placeholder]) || 5;
 }
+var maxthreads = parseFloat(process.argv[placeholder]) || 3;
+maxthreads++;  //dunno why - was getting -1 threads.
 
 //Max number of messages per thread to retrieve
 if (process.argv.indexOf('--maxmessages') > -1) {
 	var placeholder2 = process.argv.indexOf('--maxmessages');
 	placeholder2++;
-	var maxmessages = parseFloat(process.argv[placeholder2]) || 7;
 }
+var maxmessages = parseFloat(process.argv[placeholder2]) || 5;
 
-if (process.argv.indexOf('-h') > -1) {
+if (process.argv.indexOf('--help') > -1) {
+	console.log('###############################################################'.green);	
 	console.log('Without variables, will default to the most recent unread SMS');
-	console.log('Max messages is parsed before read or unread');
+	console.log('--maxmessages [NUMBER]');
+	console.log('--maxthreads [NUMBER]');
+	console.log('--getread');
+	console.log('--markread');
+	console.log('--interactive');
+	console.log('--help');
 	process.exit();
 }
-
 
 // http://st-on-it.blogspot.com/2011/05/how-to-read-user-input-with-nodejs.html
 function ask(question, format, callback) {
@@ -68,11 +75,14 @@ function ask(question, format, callback) {
  });
 }
 
+var threadnumber =0;
+
 // Get the X latest UNREAD sms conversations and display their threads, from first text to last
 client.get('sms', {limit:maxthreads}, function(error, response, data){
 	if(error){	return console.log(error);	}
 	if(!data || !data.conversations_response || !data.conversations_response.conversationgroup){ return console.log('No conversations.')}
-	console.log('SMS: Latest conversations.');
+	console.log('###############################################################'.green);
+	console.log('SMS: Latest ' +maxthreads+ ' conversations with ' +maxmessages+ ' messages in each thread.');
 	data.conversations_response.conversationgroup.forEach(function(convo){
 		//test string area here.
 		//console.log('\n', convo.conversation.phone_number);
@@ -80,9 +90,11 @@ client.get('sms', {limit:maxthreads}, function(error, response, data){
 		if (convo.conversation.label.indexOf(which) > -1) {
 			//push to array of ids for later use
 			ids.push(convo.conversation.id)
+			
 			//kludge to limit number of messages presented in each convo
 			var i = 0;
-			console.log('\n', convo.conversation.status == 1 ? ' ' : '+'.red, new Date(convo.conversation.conversation_time).toDateString().green, convo.call[0].phone_number.bold.blue, convo.conversation.id);
+			console.log('\nThread'.red,threadnumber, convo.conversation.status == 1 ? ' ' : '+'.red, new Date(convo.conversation.conversation_time).toDateString().green, convo.call[0].phone_number.bold.blue, convo.conversation.id);
+			threadnumber++;
 			convo.call.reverse().forEach(function(msg){
 			//limiting amount shown - some sms threads can be 100+ messages long.
 			if (i < maxmessages){
@@ -98,13 +110,32 @@ client.get('sms', {limit:maxthreads}, function(error, response, data){
 			});
 		}
 	});
-	
-	
+	if (markread !== 0){
+		client.set('mark', {read: true, id: ids}, function(error, response, data){
+			if(error){ return console.log(error); }
+			console.log('\nMarked conversations as read successfully.'.bold.red);
+		});	
+	};
+	// end non-interactive part //
 	//put in opportunity to reply here
-	
-	//client.set('mark', {read: true, id: ids}, function(error, response, data){
-		//if(error){ return console.log(error); }
-	//	console.log('\nMarked conversations as read successfully.');
-	//});	
-	
+	if (process.argv.indexOf('--interactive') > -1) {
+		console.log('###############################################################'.green);
+		ask("Using the Thread ID number, which thread would you care to reply to?", /.+/, function(name) {
+			if (ids[name] != undefined){
+				client.get('byId', {id: ids[name]}, function(error, response, data){
+					if(error){	return console.log(error);	}
+					data.conversations_response.conversationgroup.forEach(function(convo){
+						var to = convo.conversation.phone_number;
+						ask("Your reply is?", /.+/, function(text) {
+							client.sms({ to: to, text: text}, function(err, res, data){
+								if(err){return console.log(err);}
+								console.log('SMS "' +text+ '" sent to', to);
+								process.exit();
+							});
+						});
+					});
+				});	
+			} else { console.log('That is not a valid thread.'); process.exit();};
+		});	
+	}
 });
