c244e5fe95788dc815f553ee35c38fea0aea656a
uriel1998 <steven.saus@gmail.com>
2013-11-14 20:50:44 -0500

    Added argument handling, getting read and unread, started code for marking read.

--- get-sms.js
+++ get-sms.js
@@ -16,9 +16,56 @@ var client = new voicejs.Client({
 
 //Array for replying, marking read, etc.
 var ids = [];
-	
+
+if (process.argv.indexOf('--markread') > -1) {
+	//this means that we will automatically mark things as read
+	console.log('Marking Read');
+}
+
+if (process.argv.indexOf('--getread') > -1) {
+	var which = 'sms';
+	} else {
+	var which = 'unread';
+	}
+
+if (process.argv.indexOf('--maxthreads=') > -1) {
+	//To slice and get max number of threads to retrieve - WHETHER READ OR NOT
+	//console.log('Marking Read');
+}
+
+if (process.argv.indexOf('--maxmessages=') > -1) {
+	//To slice and get max number of messages per thread to retrieve
+	//console.log('Marking Read');
+}
+
+if (process.argv.indexOf('-h') > -1) {
+	console.log('Without variables, will default to the most recent unread SMS');
+	console.log('Max messages is parsed before read or unread');
+	process.exit();
+}
+
+
+// http://st-on-it.blogspot.com/2011/05/how-to-read-user-input-with-nodejs.html
+function ask(question, format, callback) {
+ var stdin = process.stdin, stdout = process.stdout;
+ 
+ stdin.resume();
+ stdout.write(question + ": ");
+ 
+ stdin.once('data', function(data) {
+   data = data.toString().trim();
+ 
+   if (format.test(data)) {
+     callback(data);
+   } else {
+     stdout.write("It should match: "+ format +"\n");
+     ask(question, format, callback);
+   }
+ });
+}
+
 // Get the X latest UNREAD sms conversations and display their threads, from first text to last
-client.get('unread', {limit:3}, function(error, response, data){
+client.get('sms', {limit:3}, function(error, response, data){
 	if(error){	return console.log(error);	}
 	if(!data || !data.conversations_response || !data.conversations_response.conversationgroup){ return console.log('No conversations.')}
 	console.log('SMS: Latest conversations.');
@@ -26,12 +73,12 @@ client.get('unread', {limit:3}, function(error, response, data){
 		//test string area here.
 		//console.log('\n', convo.conversation.phone_number);
 		
-		if (convo.conversation.label.indexOf('sms') > -1) {
+		if (convo.conversation.label.indexOf(which) > -1) {
 			//push to array of ids for later use
 			ids.push(convo.conversation.id)
 			//kludge to limit number of messages presented in each convo
 			var i = 0;
-			console.log('\n', convo.conversation.status == 1 ? ' ' : '+', new Date(convo.conversation.conversation_time).toDateString(), convo.call[0].phone_number, convo.conversation.id);
+			console.log('\n', convo.conversation.status == 1 ? ' ' : '+'.green, new Date(convo.conversation.conversation_time).toDateString().green, convo.call[0].phone_number.bold.blue, convo.conversation.id);
 			convo.call.reverse().forEach(function(msg){
 			//limiting amount shown - some sms threads can be 100+ messages long.
 			if (i < 7){
@@ -47,4 +94,14 @@ client.get('unread', {limit:3}, function(error, response, data){
 			});
 		}
 	});
+	
+// Get user input if to mark the messages read (if not in argv already)	
+	
+	
+	
+	//client.set('mark', {star: true, id: ids}, function(error, response, data){
+		//if(error){ return console.log(error); }
+	//	console.log('\nStarred 5 conversations successfully.');
+	//});	
+	
 });
